8c024b7af812f1498e2dbeda11b26d6c952c195c
Myroslav5 <myroslav.mykytenko5@gmail.com>
2025-12-12 01:13:46 +0200

    7th

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ItemsListComponent } from './features/products/items-list/items-list.component';
+import { ItemDetailsComponent } from './features/products/item-details/item-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'items', pathMatch: 'full' },
+  
+  { path: 'items', component: ItemsListComponent },
+  
+  { path: 'items/:id', component: ItemDetailsComponent }
+];
---
+++ src/app/core/services/products.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../../core/models/product.interface';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  
+  private allProducts: Product[] = [
+    {
+      id: 1,
+      name: 'iPhone 15 Pro',
+      price: 45000,
+      image: 'https://content2.rozetka.com.ua/goods/images/big/592111676.jpg',
+      inStock: true,
+      discount: 10
+    },
+    {
+      id: 2,
+      name: 'MacBook Air M2',
+      price: 52000,
+      image: 'https://content.rozetka.com.ua/goods/images/big/486195990.jpg',
+      inStock: true
+    },
+    {
+      id: 3,
+      name: 'AirPods Pro 2',
+      price: 11000,
+      image: 'https://content.rozetka.com.ua/goods/images/big/594443650.jpg',
+      inStock: false
+    },
+    {
+      id: 4,
+      name: 'Sony PlayStation 5',
+      price: 24000,
+      image: 'https://content2.rozetka.com.ua/goods/images/big/529294298.jpg',
+      inStock: true,
+      discount: 5
+    }
+  ];
+
+  private productsSubject = new BehaviorSubject<Product[]>(this.allProducts);
+
+  constructor() { }
+
+  getProducts(): Observable<Product[]> {
+    return this.productsSubject.asObservable();
+  }
+
+  filterProducts(searchQuery: string): void {
+    const lowerQuery = searchQuery.toLowerCase();
+    
+    const filtered = this.allProducts.filter(product => 
+      product.name.toLowerCase().includes(lowerQuery)
+    );
+
+    this.productsSubject.next(filtered);
+  }
+  getProductById(id: number): Product | undefined {
+    return this.allProducts.find(product => product.id === id);
+  }
+}
